506f4810c38e107515543442d0b6f04949127adf
musis <feativa@gmail.com>
2022-04-07 21:57:44 -0300

    Commit initial

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ extension CompactMap<T> on Iterable<T?> {
   Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
       map(transform ?? (e) => e).where((element) => element != null).cast();
 }
-
+/// Testando hooks para animações.
 void main() {
   runApp(const MyApp());
 }
